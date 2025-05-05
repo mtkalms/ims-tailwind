8a76925988794fef2756fc99e7e0be6257ae49a8
Matthias Kalms <mtkalms@gmail.com>
2025-05-04 20:58:43 -0700

    Basic design for project pages

--- pages/[project]/index.tsx
+++ pages/[project]/index.tsx
@@ -23,11 +23,16 @@ function Project() {
         };
 
   return (
-    <div>
-      {projectId}
-      <span>{project.name}</span>
-      <h1>{project.Name}</h1>
-      <p>{project.description}</p>
+    <div className="relative overflow-x-auto shadow-md flex flex-col gap-4">
+      <div className="flex items-center gap-2 justify-between border-b border-slate-600 pb-4">
+        <h1 className="flex gap-2 text-2xl">
+          {project?.name}
+        </h1>
+        <span className="font-bold">{project?.status}</span>
+      </div>
+      <div className="text-sm ">
+        <span className="font-bold">Description:</span> {project?.description}
+      </div>
     </div>
   );
 }
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,31 +2,28 @@ import { StoreContext } from "@/contexts/StoreContext";
 import { useContext } from "react";
 import Link from "next/link";
 import { useTable } from "tinybase/ui-react";
+import { IconTable } from "@tabler/icons-react";
 
 function ProjectsPage() {
   const storeContext = useContext(StoreContext);
   return (
-    <div>
-      <h1>Projects</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Object.entries(useTable("projects", storeContext?.store)).map(
-            ([id, project]) => (
-              <tr key={id}>
-                <td><Link href={`/${id}`}>{project.name}</Link></td>
-                <td>{project.description}</td>
-              </tr>
-            )
-          )}
-        </tbody>
-      </table>
-    </div>
+    <ul className="relative overflow-x-auto shadow-md rounded-lg text-sm border border-slate-600 flex flex-col">
+      {Object.entries(useTable("projects", storeContext?.store)).map(([id, project]) => (
+        <li className=" p-3 [&:not(:last-child)]:border-b border-slate-600 hover:bg-slate-800" key={id}>
+          <Link href={`/${id}`}>
+            <div className="flex gap-3">
+              <IconTable className="stroke-slate-400 w-5"/>
+              <div className="flex flex-col">
+                <span className="font-bold">{project.name}</span>
+                <div className="flex items-center gap-2 text-slate-400 text-xs">
+                  <span>{project.description}</span>
+                </div>
+              </div>
+            </div>
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 }
 export default ProjectsPage;
