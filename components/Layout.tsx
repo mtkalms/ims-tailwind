6585662f006dc3489ffab30a9edf60277ed3e95e
Matthias Kalms <mtkalms@gmail.com>
2025-05-04 18:29:13 -0700

    Also make Sidebar dynamic

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,13 +3,7 @@ import Sidebar from "./Sidebar/Sidebar";
 import Navbar from "./Navbar";
 import { IconBolt, IconHome, IconTable } from "@tabler/icons-react";
 import { useRouter } from "next/router";
-import { Url } from "next/dist/shared/lib/router/router";
 import { getIncidentPaths } from "@/static/paths";
-
-interface NavbarProps {
-  children?: React.ReactNode;
-  onToggleSidebar?: () => void;
-}
 
 export const getStaticPaths = async () => ({
   paths: getIncidentPaths(),
@@ -22,33 +16,71 @@ interface ProjectNavMenuProps {
 }
 
 function ProjectNavMenu({ project }: ProjectNavMenuProps) {
-  return <Navbar.Menu>
-    <Navbar.MenuItem
-      label="Project"
-      href={`/${project}`}
-      icon={<IconTable className="icon-outline"/>}
-    />
-    <Navbar.MenuItem
-      label="Incidents"
-      href={`/${project}/incidents`}
-      icon={<IconBolt className="icon-outline"/>}
-    />
-  </Navbar.Menu>
+  return (
+    <Navbar.Menu>
+      <Navbar.MenuItem
+        label="Project"
+        href={`/${project}`}
+        icon={<IconTable className="icon-outline" />}
+      />
+      <Navbar.MenuItem
+        label="Incidents"
+        href={`/${project}/incidents`}
+        icon={<IconBolt className="icon-outline" />}
+      />
+    </Navbar.Menu>
+  );
 }
 
 function NavMenu() {
-  return <Navbar.Menu>
-    <Navbar.MenuItem
-      label="Home"
-      href="/"
-      icon={<IconHome className="icon-outline"/>}
-    />
-    <Navbar.MenuItem
-      label="Projects"
-      href="/projects"
-      icon={<IconTable className="icon-outline"/>}
-    />
-  </Navbar.Menu>
+  return (
+    <Navbar.Menu>
+      <Navbar.MenuItem
+        label="Home"
+        href="/"
+        icon={<IconHome className="icon-outline" />}
+      />
+      <Navbar.MenuItem
+        label="Projects"
+        href="/projects"
+        icon={<IconTable className="icon-outline" />}
+      />
+    </Navbar.Menu>
+  );
+}
+
+function ProjectSidebarMenu({ project }: ProjectNavMenuProps) {
+  return (
+    <Sidebar.Menu>
+      <Sidebar.MenuItem
+        label="Project"
+        href={`/${project}`}
+        icon={<IconTable className="icon-outline" />}
+      />
+      <Sidebar.MenuItem
+        label="Incidents"
+        href={`/${project}/incidents`}
+        icon={<IconBolt className="icon-outline" />}
+      />
+    </Sidebar.Menu>
+  );
+}
+
+function SidebarMenu() {
+  return (
+    <Sidebar.Menu>
+      <Sidebar.MenuItem
+        label="Home"
+        href="/"
+        icon={<IconHome className="icon-outline" />}
+      />
+      <Sidebar.MenuItem
+        label="Projects"
+        href="/projects"
+        icon={<IconTable className="icon-outline" />}
+      />
+    </Sidebar.Menu>
+  );
 }
 
 function Layout({ children }: { children: React.ReactNode }) {
@@ -59,26 +91,13 @@ function Layout({ children }: { children: React.ReactNode }) {
 
   return (
     <div>
-      <Navbar onToggleSidebar={() => setShowSidebar(value => !value)}>
+      <Navbar onToggleSidebar={() => setShowSidebar((value) => !value)}>
         {project ? <ProjectNavMenu project={project} /> : <NavMenu />}
       </Navbar>
       <Sidebar show={showSidebar} onClose={() => setShowSidebar(false)}>
-        <Sidebar.Menu>
-          <Sidebar.MenuItem
-            label="Home"
-            href="/"
-            icon={<IconHome className="icon-outline"/>}
-          />
-          <Sidebar.MenuItem
-            label="Projects"
-            href="/projects"
-            icon={<IconTable className="icon-outline"/>}
-          />
-        </Sidebar.Menu>
+        {project ? <ProjectSidebarMenu project={project} /> : <SidebarMenu />}
       </Sidebar>
-      <div className="mx-auto grid w-full max-w-7xl p-8">
-        {children}
-      </div>
+      <div className="mx-auto grid w-full max-w-7xl p-8">{children}</div>
     </div>
   );
 }
