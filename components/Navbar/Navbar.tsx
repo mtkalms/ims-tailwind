154288f3cd8d72525f481e1e85a6693b7c0a7c5f
Matthias Kalms <mtkalms@gmail.com>
2025-05-04 18:48:54 -0700

    Add home button to Navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,7 +1,8 @@
-import { IconMenu2 } from "@tabler/icons-react";
+import { IconMenu2, IconMeteor } from "@tabler/icons-react";
 import ThemeModeToggle from "../ThemeModeToggle";
 import NavbarMenu from "./NavbarMenu";
 import NavbarMenuItem from "./NavbarMenuItem";
+import Link from "next/link";
 
 interface NavbarProps {
   children?: React.ReactNode;
@@ -9,22 +10,24 @@ interface NavbarProps {
 }
 
 function Navbar({ children, onToggleSidebar }: NavbarProps) {
-
   return (
     <div className="navbar border-b-1 border-slate-700 flex flex-col">
       <div className="flex items-center justify-between p-4 pb-0">
-        <div className="flex items-center">
+        <div className="flex items-center gap-4">
           <button onClick={onToggleSidebar} className="navbar-toggle button">
-            <IconMenu2 className="icon-outline"/>
+            <IconMenu2 className="icon-outline" />
           </button>
+          <div className="rounded-full bg-purple-600 p-1">
+            <Link href="/">
+              <IconMeteor className="stroke-amber-400" />
+            </Link>
+          </div>
         </div>
         <div className="flex items-center">
-          <ThemeModeToggle/>
+          <ThemeModeToggle />
         </div>
       </div>
-      <nav className="flex items-center pt-2 pb-0">
-        {children}
-      </nav>
+      <nav className="flex items-center pt-2 pb-0">{children}</nav>
     </div>
   );
 }
@@ -34,4 +37,3 @@ Navbar.MenuItem = NavbarMenuItem;
 
 export default Navbar;
 export type { NavbarProps };
-
