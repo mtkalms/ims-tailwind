19dfdab83f39b9751b4bf55f1a97136564f3cbe7
Matthias Kalms <mtkalms@gmail.com>
2025-05-05 22:33:29 -0700

    Test if action successfully sets basePath

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from "next";
 
-const isGitHubPages = process.env.GITHUB_PAGES === 'true';
-
-console.log('GitHub Pages Build:', isGitHubPages);
-
 const nextConfig: NextConfig = {
   output: 'export',
   reactStrictMode: true,
-  assetPrefix: isGitHubPages ? '/ims-tailwind/' : undefined,
-  basePath: isGitHubPages ? '/ims-tailwind' : '',
   images: {
     unoptimized: true,
   },
